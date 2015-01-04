a8bb0747a8baf50510b43841338e9788618c454e
Martin Keogh <martin1keogh@gmail.com>
2015-01-04 11:03:02 +0100

    Remove irssi conf from install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ ZSH_INSTALLED=false
 ### GIT ###
 if test `which git`; then
 	GIT_INSTALLED=true
-	cp -r gitconfig $HOME/.gitconfig
-	cp -r git_template $HOME/.git_template
+	cp -rn gitconfig $HOME/.gitconfig
+	cp -rn git_template $HOME/.git_template
 else 
 	echo "Couldn't find git, this might cause problems for further installs."
 	echo "Continue ? (y/N)"
@@ -23,10 +23,10 @@ fi
 ### ZSH ###
 if test `which zsh`; then
 	ZSH_INSTALLED=true
-	cp -r zshrc $HOME/.zshrc
+	cp -rn zshrc $HOME/.zshrc
 	if $GIT_INSTALLED; then
 		git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-		cp -r oh-my-zsh $HOME/.oh-my-zsh
+		cp -rn oh-my-zsh $HOME/.oh-my-zsh
 	else
 		echo "Couldn't find git, skipping `oh-my-zsh`'s install."
 	fi
@@ -37,7 +37,7 @@ fi
 ### POWERLINE ###
 if test `which powerline`; then
 	POWERLINE_INSTALLED=true
-	cp -r powerline $HOME/.config/
+	cp -rn powerline $HOME/.config/
 else 
 	echo "Couldn't find powerline, this will cause bugs in vim/tmux"
 	echo "Continue ? (y/N)"
@@ -53,7 +53,7 @@ fi
 
 ### TMUX ###
 if test `which tmux`; then
-	cp -r tmux.conf $HOME/.tmux.conf
+	cp -rn tmux.conf $HOME/.tmux.conf
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
 	echo "Tmux no found, skipping .tmux.conf"
@@ -61,19 +61,14 @@ fi
 
 ### VIM ###
 if test `which vim`; then
-	cp -r vim $HOME/.vim
-	cp -r vimrc $HOME/.vimrc
+	cp -rn vim $HOME/.vim
+	cp -rn vimrc $HOME/.vimrc
 	ln -s $HOME/.vim/resources/colors $HOME/.vim/colors
 	mkdir -p ~/.vim/bundle
 	git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 	vim -u $HOME/.vimrc +NeoBundleUpdate +qa
 else 
 	echo "Couldn't find vim. Please install it and ensure it's 7.3+ with python enabled."
-fi
-
-### IRSSI ###
-if test `which irssi`; then
-	cp -r irssi $HOME/.irssi
 fi
 
 ### Change user shell to zsh
