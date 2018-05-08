7e1691edf3d277f52af85d0f79efb99716e56751
Martin Keogh <martin1keogh@gmail.com>
2018-05-08 23:31:15 +0200

    Rework the install script
    Install dependencies in the script.
    Log successful steps to avoid retrying those.
    Use neovim instead of vim.
    Fix wrong symbolic link path in zsh custom files

--- install.sh
+++ install.sh
@@ -1,89 +1,81 @@
-GIT_INSTALLED=false
-POWERLINE_INSTALLED=false
-ZSH_INSTALLED=false
+#!/bin/bash
+
+set -x
+set -e
+set -u
 
 SCRIPT=`readlink -f $0`
 SCRIPTDIR=`dirname "$SCRIPT"`
 
-### GIT ###
-if test `which git`; then
-	GIT_INSTALLED=true
-	ln -T -s $SCRIPTDIR/gitconfig $HOME/.gitconfig
-	ln -T -s $SCRIPTDIR/git_template $HOME/.git_template
-else 
-	echo "Couldn't find git, this might cause problems for further installs."
-	echo "Continue ? (y/N)"
-	read -r ans
-	case $ans in
-		[yY]|[yY]es)
-			;;
-		*)
-			exit 0
-			;;
-	esac
-fi
+# Creates a symbolic link of the file to $HOME
+# If as second argument is passed, creates the link
+# in $HOME/$2 instead.
+link() {
+  ln -s $SCRIPTDIR/$1 $HOME/${2:-.}/.$1
+}
+
+install_if_needed() {
+  log_file=$SCRIPTDIR/.successful_steps
+  echo "${@:2}"
+  grep $1 $log_file > /dev/null || {
+    eval "${@:2}" && echo $1 >> $log_file
+  }
+}
+
+install_if_needed "aptitude"  \
+  sudo apt-get install -y aptitude \;\
+  sudo aptitude install \
+    zsh neovim git tmux mosh \
+    python-pip curl tig
+
+
+# GIT
+install_if_needed "git_and_tig" \
+  link gitconfig \;\
+  link git_template \;\
+  link tigrc
 
 ### ZSH ###
-if test `which zsh`; then
-	ZSH_INSTALLED=true
-	ln -T -s $SCRIPTDIR/zshrc $HOME/.zshrc
-	if $GIT_INSTALLED; then
-		git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-		for custom in `ls $SCRIPTDIR/oh-my-zsh/custom/*`; do
-		  ln -T -s $SCRIPTDIR/oh-my-zsh/custom/$custom $HOME/.oh-my-zsh/custom/`basename $custom`
-		done
-		for theme in `ls $SCRIPTDIR/oh-my-zsh/themes/*`; do
-		  ln -T -s $SCRIPTDIR/oh-my-zsh/themes/$theme $HOME/.oh-my-zsh/themes/`basename $theme`
-		done
-	else
-		echo "Couldn't find git, skipping `oh-my-zsh`'s install."
-	fi
-else 
-	echo "Couldn't find zsh, please install it and then restart the installer."
-fi
+install_if_needed "zsh" \
+  link zshrc \;\
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+for custom in $SCRIPTDIR/oh-my-zsh/custom/*; do
+  ln -b -T -s $custom $HOME/.oh-my-zsh/custom/$(basename $custom)
+done
+for theme in $SCRIPTDIR/oh-my-zsh/themes/*; do
+  ln -b -T -s $theme $HOME/.oh-my-zsh/themes/$(basename $theme)
+done
 
 ### POWERLINE ###
-if test `which powerline`; then
-	POWERLINE_INSTALLED=true
-	ln -T -s $SCRIPTDIR/powerline $HOME/.config/
-else 
-	echo "Couldn't find powerline, this will cause bugs in vim/tmux"
-	echo "Continue ? (y/N)"
-	read -r ans
-	case $ans in
-		[yY]|[yY]es)
-			;;
-		*)
-			exit 0
-			;;
-	esac
-fi
+install_if_needed "powerline" \
+  pip install --user powerline-status \;\
+  link powerline .config
 
 ### TMUX ###
-if test `which tmux`; then
-	ln -T -s $SCRIPTDIR/tmux.conf $HOME/.tmux.conf
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-else
-	echo "Tmux no found, skipping .tmux.conf"
-fi
+install_if_needed "tmux" \
+  link tmux.conf \;\
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-### VIM ###
-if test `which vim`; then
-	ln -T -s $SCRIPTDIR/vim $HOME/.vim
-	ln -T -s $SCRIPTDIR/vimrc $HOME/.vimrc
-	mkdir -p $HOME/.vim_bundle
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	vim -u $HOME/.vimrc +PlugInstall +qa
-	mkdir $HOME/.vim_undo
-else 
-	echo "Couldn't find vim. Please install it and ensure it's 7.3+ with python enabled."
-fi
-
-### Change user shell to zsh
-if $ZSH_INSTALLED; then
-	echo "Do not forget to change your default shell to zsh! (`chsh -s /bin/zsh`)"
-fi
+### NEOVIM ###
+NVIM_HOME=~/.config/nvim
+mkdir -p $NVIM_HOME
+mkdir -p $HOME/.vim_bundle
+mkdir -p $HOME/.vim_undo
+install_if_needed "neovim" \
+  ln -s $SCRIPTDIR/init.vim $NVIM_HOME \;\
+  ln -s $SCRIPTDIR/vim/colors $NVIM_HOME/colors \;\
+  link vim \;\
+  curl -fLo $NVIM_HOME/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \;\
+  nvim +PlugInstall +qa
 
 ### pryrc
-ln -T -s $SCRIPTDIR/pryrc $HOME/.pryrc
-ln -T -s $SCRIPTDIR/irbrc $HOME/.irbrc
+install_if_needed "psqlrc" link psqlrc
+install_if_needed "pryrc" link pryrc
+install_if_needed "irbrc" link irbrc
+
+echo
+echo
+echo
+echo Changing shell to zsh
+chsh -s /usr/bin/zsh
