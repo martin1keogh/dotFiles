27f80b6001dbfc5e300bf9ed107fedfe33b747d0
Martin Keogh <martin1keogh@gmail.com>
2018-05-08 21:34:45 +0200

    Add silversearcher-ag to default packages

--- install.sh
+++ install.sh
@@ -26,7 +26,8 @@ install_if_needed "aptitude"  \
   sudo apt-get install -y aptitude \;\
   sudo aptitude install \
     zsh neovim git tmux mosh \
-    python-pip python3-pip curl tig
+    python-pip python3-pip curl tig \
+    silversearcher-ag
 
 
 # GIT
