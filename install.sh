fded9cda1d266d9c19da703dcc4945cf2c5e4b0a
Martin Keogh <martin1keogh@gmail.com>
2020-01-02 14:06:34 +0100

    fix: quote variables in shell script (SC2086)

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ set -x
 set -e
 set -u
 
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname "$SCRIPT"`
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPT")
 
 # Creates a symbolic link of the file to $HOME
 # If as second argument is passed, creates the link
 # in $HOME/$2 instead.
 link() {
-  ln -s $SCRIPTDIR/$1 $HOME/${2:-.}/.$1
+  ln -s "$SCRIPTDIR"/"$1" "$HOME"/"${2:-.}"/."$1"
 }
 
 install_if_needed() {
   log_file=$SCRIPTDIR/.successful_steps
   echo "${@:2}"
-  grep $1 $log_file > /dev/null || {
-    eval "${@:2}" && echo $1 >> $log_file
+  grep "$1" "$log_file" > /dev/null || {
+    eval "${@:2}" && echo "$1" >> "$log_file"
   }
 }
 
@@ -41,11 +41,11 @@ install_if_needed "zsh" \
   link zshrc \;\
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-for custom in $SCRIPTDIR/oh-my-zsh/custom/*; do
-  ln -b -T -s $custom $HOME/.oh-my-zsh/custom/$(basename $custom)
+for custom in "$SCRIPTDIR"/oh-my-zsh/custom/*; do
+  ln -b -T -s "$custom" "$HOME"/.oh-my-zsh/custom/"$(basename "$custom")"
 done
-for theme in $SCRIPTDIR/oh-my-zsh/themes/*; do
-  ln -b -T -s $theme $HOME/.oh-my-zsh/themes/$(basename $theme)
+for theme in "$SCRIPTDIR"/oh-my-zsh/themes/*; do
+  ln -b -T -s "$theme" "$HOME"/.oh-my-zsh/themes/"$(basename "$theme")"
 done
 
 git clone 'git@github.com:martin1keogh/zsh_pgpass_completion.git' ~/.oh-my-zsh/custom/plugins/psql/
@@ -62,17 +62,16 @@ install_if_needed "tmux" \
 ### NEOVIM ###
 NVIM_HOME=~/.config/nvim
 mkdir -p $NVIM_HOME
-mkdir -p $HOME/.vim_bundle
-mkdir -p $HOME/.vim_undo
+mkdir -p "$HOME"/.vim_bundle
+mkdir -p "$HOME"/.vim_undo
 install_if_needed "neovim" \
-  ln -s $SCRIPTDIR/init.vim $NVIM_HOME \;\
-  ln -s $SCRIPTDIR/vim/colors $NVIM_HOME/colors \;\
+  ln -s "$SCRIPTDIR"/init.vim $NVIM_HOME \;\
+  ln -s "$SCRIPTDIR"/vim/colors $NVIM_HOME/colors \;\
   link vim \;\
   curl -fLo $NVIM_HOME/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \;\
   pip3 install --user neovim \;\
   nvim +PlugInstall +UpdateRemovePlugins +qa
 
-### pryrc
 install_if_needed "psqlrc" link psqlrc
 install_if_needed "pryrc" link pryrc
 install_if_needed "irbrc" link irbrc
