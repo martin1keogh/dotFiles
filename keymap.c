9817f1e45eedb60d8dfb2fe37bcc9434083fa93d
Martin Keogh <martin.keogh@easyence.com>
2022-05-03 14:45:28 +0200

    refacto: rework corne keyboard keymap.c

--- keymap.c
+++ keymap.c
@@ -28,28 +28,30 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 #define L_BASE 0
-#define L_SYMBOL 2
-#define L_FUNCTION 4
-#define L_ADJUST 8
+#define L_SYMBOL 1
+#define L_NAVIGATION 2
+#define L_FUNCTION 3
+#define L_ADJUST 4
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR(" Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Normal"), false);
-            break;
-        case L_SYMBOL:
-            oled_write_ln_P(PSTR("Symbol"), false);
-            break;
-        case L_FUNCTION:
-            oled_write_ln_P(PSTR("Function"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_SYMBOL:
-        case L_ADJUST|L_FUNCTION:
-        case L_ADJUST|L_SYMBOL|L_FUNCTION:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
+    if (IS_LAYER_ON(L_ADJUST)) {
+        oled_write_ln_P(PSTR("Adjust"), false);
+    }
+    else if (IS_LAYER_ON(L_FUNCTION)) {
+        oled_write_ln_P(PSTR("Function"), false);
+    }
+    else if (IS_LAYER_ON(L_NAVIGATION)) {
+        oled_write_ln_P(PSTR("Navigation"), false);
+    }
+    else if (IS_LAYER_ON(L_SYMBOL)) {
+        oled_write_ln_P(PSTR("Symbol"), false);
+    }
+    else if (IS_LAYER_ON(L_BASE)) {
+        oled_write_ln_P(PSTR("Normal"), false);
+    }
+    else {
+        oled_write_ln_P(PSTR("Unknown"), false);
     }
 }
 
@@ -144,51 +146,51 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
+  [L_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
 COMPOSE_KEY_MACRO,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LGUI,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, MO(L_FUNCTION),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-	                            MO(3), LSFT_T(KC_BSPC), LCTL_T(KC_ESC),   LALT_T(KC_ENT), LT(1, KC_SPC), MO(2)
+	                               KC_BSPC, KC_LSFT, LCTL_T(KC_ESC),     LALT_T(KC_ENT), LT(L_SYMBOL, KC_SPC), MO(L_NAVIGATION)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-  [1] = LAYOUT_split_3x6_3(
+  [L_SYMBOL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	    KC_TRNS, KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_AMPR,                      KC_ASTR,    KC_7,    KC_8,    KC_9, KC_PPLS, KC_BSPC,
+	    KC_TRNS, KC_EXLM,   KC_AT, KC_LPRN, KC_RPRN, KC_AMPR,                      KC_ASTR,    KC_7,    KC_8,    KC_9, KC_PPLS, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_TRNS, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN, KC_MINS,                       KC_EQL,    KC_4,    KC_5,    KC_6, KC_MINS,   KC_NO,
+	    KC_TRNS, KC_HASH,  KC_DLR, KC_LBRC, KC_RBRC, KC_MINS,                       KC_EQL,    KC_4,    KC_5,    KC_6, KC_MINS, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV ,                       KC_DOT,    KC_1,    KC_2,    KC_3,    KC_0,   KC_NO,
+	    KC_TRNS, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_GRV ,                       KC_DOT,    KC_1,    KC_2,    KC_3,    KC_0,   MO(1),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-	                                        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_0
+	                                        KC_DEL, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_0
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [2] = LAYOUT_split_3x6_3(
+  [L_NAVIGATION] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	    KC_TRNS, KC_F9,    KC_F10, KC_F11,   KC_F12, KC_VOLU,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,   KC_NO,
+      KC_TRNS, KC_TRNS, KC_TRNS,  KC_ENT, KC_TRNS, KC_VOLU,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,   RESET,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_VOLD,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,   KC_NO,
+KC_TRNS,OSM(MOD_LCTL),OSM(MOD_LALT),OSM(MOD_LSFT),OSM(MOD_LGUI),KC_VOLD,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_MUTE,                        KC_NO  , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+	    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,                        KC_NO  , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-	                                        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
+	                                      KC_TRNS,KC_TRNS,OSL(L_FUNCTION),  KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
 
-  [3] = LAYOUT_split_3x6_3(
+  [L_FUNCTION] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	    RESET, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
+	    KC_TRNS, KC_F9,   KC_F10 , KC_F11,   KC_F12, KC_VOLU,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
+	    KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_VOLD,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+	    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_MUTE,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 	                                        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
